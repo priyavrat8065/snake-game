971bd228be1a20f50f09f38449c22c05817ed6f3
Priyavrat Chaudhary <priyavratchaudhary862@gmail.com>
2021-12-02 16:54:48 +0530

    make notes

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ screen.setup(width=600, height=600)  # using keyword arguments
 screen.bgcolor("black")
 screen.title("My snake game")
 screen.exitonclick()
+# TODO: 1) Create a snake body
+# when we create a turtle object. It is 20 x 20 pixels in dimension.
