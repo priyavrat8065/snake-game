6aff9bb99d68a2b3f0cfdba1216edd87c72a1215
Priyavrat Chaudhary <priyavratchaudhary862@gmail.com>
2021-12-02 16:50:46 +0530

    set up main.py and .gitignore file

---
+++ main.py
@@ -0,0 +1,7 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)  # using keyword arguments
+screen.bgcolor("black")
+screen.title("My snake game")
+screen.exitonclick()
