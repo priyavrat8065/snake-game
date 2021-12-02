6f5ab8d4f529ea78d2d77bf2f3e0ee9721f81639
Priyavrat Chaudhary <priyavratchaudhary862@gmail.com>
2021-12-02 17:05:21 +0530

    create snake body

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ screen = Screen()
 screen.setup(width=600, height=600)  # using keyword arguments
 screen.bgcolor("black")
 screen.title("My snake game")
-screen.exitonclick()
 # TODO: 1) Create a snake body
 # when we create a turtle object. It is 20 x 20 pixels in dimension.
+snake = []
+gap = 0
+for snake_part in range(0, 3):
+    new_part = Turtle("square")
+    new_part.color("white")
+    new_part.goto(x=0 + gap, y=0)
+    gap -= 20
+    snake.append(new_part)
+
+
+
+
+
+screen.exitonclick()
