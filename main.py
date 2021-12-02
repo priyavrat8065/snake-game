74b7857a98fff54452353b10e2736bd5478fa59f
Priyavrat Chaudhary <priyavratchaudhary862@gmail.com>
2021-12-02 22:04:53 +0530

    make snake move in such a way its each segment is dependent on the one right before it

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ screen.tracer(0)  # it is a kind of parda that hide what is happening behind the
 # TODO: 1) Create a snake body
 # when we create a turtle object. It is 20 x 20 pixels in dimension.
 # Master branch contains Angela's code, my-work branch contains my code.
-snake = []
+segments = []
 gap = 0
-for snake_part in range(0, 3):
+for seg_pos in range(0, 3):
     new_part = Turtle("square")
     new_part.color("white")
     new_part.penup()
     new_part.goto(x=0 + gap, y=0)
     gap -= 20
-    snake.append(new_part)
+    segments.append(new_part)
 
 # TODO: 2) Move the snake
 # We make the snake move forward in one direction without having to do anything
@@ -27,8 +27,11 @@ game_is_on = True
 while game_is_on:
     screen.update()  # it tells when to remove that parda.
     time.sleep(0.1)
-    for snake_part in snake:
-        snake_part.fd(20)
+    for pos in range(len(segments) - 1, 0, -1):
+        new_x = segments[pos - 1].xcor()
+        new_y = segments[pos - 1].ycor()
+        segments[pos].goto(new_x, new_y)
+    segments[0].fd(20)
 
 
 
